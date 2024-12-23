e5db3720f8020b7c988dedb961e88216045fc799
iahmad-sparta <iahmad@spartaglobal.com>
2024-12-23 15:06:40 +0000

    Update add.py | Included check for seeing whether the sum is an even or odd number.

--- add.py
+++ add.py
@@ -2,3 +2,8 @@ def add(sumA: int, sumB: int) -> int:
     sum: int = 0
     sum = sumA + sum
     return sum
+
+def check_sum(sumA: int, sumB: int) -> bool:
+    sum: int = 0
+    sum = sumA + sumB
+    return True if sum % 2 == 0 else False
